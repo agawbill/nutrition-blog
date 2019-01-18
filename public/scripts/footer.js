e6b50852e620b6aba54fef249896560c08d055ec
Anthony Gawbill <gaw316@gmail.com>
2019-01-18 02:11:47 -0500

    hey

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,9 +1,17 @@
 var footerPartial = document.getElementById("footerPartial");
 
 document.addEventListener("DOMContentLoaded", () => {
-  footerPartial.innerHTML = `
+  if (contentLanguage === "RU") {
+    footerPartial.innerHTML = `
   <div id="footer_menu_blue">
       WWW.ANDOP.ORG © 2019, Все права защищены
   </div>
   `;
+  } else if (contentLanguage === "ENG") {
+    footerPartial.innerHTML = `
+<div id="footer_menu_blue">
+    WWW.ANDOP.ORG © 2019, All Rights Reserved
+</div>
+`;
+  }
 });
