79af56ef369113e9e98cafa93838eed635cdbad1
Anthony Gawbill <gaw316@gmail.com>
2019-01-18 12:30:04 -0500

    hello

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,24 +1,37 @@
 var footerPartial = document.getElementById("footerPartial");
 
+const addFooter = contentLanguage => {
+  const engNode = `<div id="footer_menu_blue">
+      WWW.ANDOP.ORG © 2019, All Rights Reserved
+  </div>`;
+
+  const rusNode = `<div id="footer_menu_blue">
+        WWW.ANDOP.ORG © 2019, Все права защищены
+    </div>`;
+
+  const azNode = `<div id="footer_menu_blue">
+    WWW.ANDOP.ORG © 2019, Bütün hüquqlar qorunur
+    </div>`;
+
+  if (contentLanguage === "RU") {
+    $("#footerPartial").append(rusNode);
+  } else if (contentLanguage === "ENG") {
+    $("#footerPartial").append(engNode);
+  } else if (contentLanguage === "AZ") {
+    $("#footerPartial").append(azNode);
+  }
+};
+
 document.addEventListener("click", () => {
   if (event.target.value === "RU") {
-    footerPartial.innerHTML = `
-  <div id="footer_menu_blue">
-      WWW.ANDOP.ORG © 2019, Все права защищены
-  </div>
-  `;
+    $("#footerPartial").empty();
+    addFooter(contentLanguage);
   } else if (event.target.value === "ENG") {
-    footerPartial.innerHTML = `
-<div id="footer_menu_blue">
-    WWW.ANDOP.ORG © 2019, All Rights Reserved
-</div>
-`;
+    $("#footerPartial").empty();
+    addFooter(contentLanguage);
   } else if (event.target.value === "AZ") {
-    footerPartial.innerHTML = `
-    <div id="footer_menu_blue">
-    WWW.ANDOP.ORG © 2019, Bütün hüquqlar qorunur
-    </div>
-    `;
+    $("#footerPartial").empty();
+    addFooter(contentLanguage);
   }
 });
 
