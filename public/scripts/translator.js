a546d11e4a369a20a443e0e220df9fbdfa14f6b4
Anthony Gawbill <gaw316@gmail.com>
2019-01-18 19:41:13 -0500

    side menu added

--- public/scripts/translator.js
+++ public/scripts/translator.js
@@ -2,6 +2,8 @@ window.onload = () => {
   var languageSelect = document.getElementById("languageSelect");
 };
 
+var sideMenu = document.getElementById("sideMenu");
+
 var contentLanguage = localStorage["myKey"] || "RU";
 var titleLanguage = localStorage["myKey"] || "RU";
 var descriptionLanguage = localStorage["myKey"] || "RU";
@@ -9,6 +11,61 @@ var descriptionLanguage = localStorage["myKey"] || "RU";
 var russian = document.getElementById("russian");
 var english = document.getElementById("english");
 var azerbaijan = document.getElementById("azerbaijan");
+
+const switchMenu = () => {
+  const russianMenu = `
+          <ul style='list-style-type: none; margin: 0; padding: 0; font-size:1.1em;'>
+           <li><a class="nav-link" href="about.html">Об Академии <span class="sr-only">(current)</span></a></li>
+            <hr>
+            </li><a class="nav-link" href="team.html">Наша Команда</a> </li>
+            <hr>
+            </li><a class="nav-link" href="projects.html">Наши Проекты</a> </li>
+            <hr>
+            </li> <a class="nav-link" href="trainings.html">Наши Тренинги</a></li>
+            <hr>
+            </li> <a class="nav-link" href="membership.html">Членство</a> </li>
+            <hr>
+          </ul>`;
+  const englishMenu = `
+          <ul style='list-style-type: none; margin: 0; padding: 0; font-size:1.1em;'>
+           <li><a class="nav-link" href="about.html">About <span class="sr-only">(current)</span></a></li>
+            <hr>
+            </li><a class="nav-link" href="team.html">Team</a> </li>
+            <hr>
+            </li><a class="nav-link" href="projects.html">Projects</a> </li>
+            <hr>
+            </li> <a class="nav-link" href="trainings.html">Trainings</a></li>
+            <hr>
+            </li> <a class="nav-link" href="membership.html">Membership</a> </li>
+            <hr>
+          </ul>`;
+  const azerbaijanMenu = `
+          <ul style='list-style-type: none; margin: 0; padding: 0; font-size:1.1em;'>
+           <li><a class="nav-link" href="about.html">ANDOP<span class="sr-only">(current)</span></a></li>
+            <hr>
+            </li><a class="nav-link" href="team.html">Komandamiz</a> </li>
+            <hr>
+            </li><a class="nav-link" href="projects.html">Proyektləri̇mi̇z</a> </li>
+            <hr>
+            </li> <a class="nav-link" href="trainings.html">Treni̇nglər</a></li>
+            <hr>
+            </li> <a class="nav-link" href="membership.html">Üzvlük</a> </li>
+            <hr>
+          </ul>`;
+
+  if (contentLanguage === "ENG") {
+    $("#sideMenu").empty();
+    $("#sideMenu").append(englishMenu);
+  } else if (contentLanguage === "RU") {
+    $("#sideMenu").empty();
+    $("#sideMenu").append(russianMenu);
+  } else {
+    $("#sideMenu").empty();
+    $("#sideMenu").append(azerbaijanMenu);
+  }
+};
+
+switchMenu(contentLanguage);
 
 const switchLanguage = () => {
   if (contentLanguage === "ENG") {
@@ -34,14 +91,17 @@ window.onload = () => {
       localStorage["myKey"] = "ENG";
       contentLanguage = "ENG";
       switchLanguage(contentLanguage);
+      switchMenu(contentLanguage);
     } else if (languageSelect.value == "RU") {
       localStorage["myKey"] = "RU";
       contentLanguage = "RU";
       switchLanguage(contentLanguage);
+      switchMenu(contentLanguage);
     } else if (languageSelect.value == "AZ") {
       localStorage["myKey"] = "AZ";
       contentLanguage = "AZ";
       switchLanguage(contentLanguage);
+      switchMenu(contentLanguage);
     }
   });
 };
