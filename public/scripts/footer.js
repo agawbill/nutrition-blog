f80e1561ea1229b1f85a0daa2f09a0781120e45d
Asim <salimli.asim@gmail.com>
2019-02-22 10:32:18 +0000

    responsive menu

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -15,7 +15,7 @@ const addFooter = contentLanguage => {
 
   const menuEng = `<nav style='font-size:16px;' class="navbar navbar-expand-lg">
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <i class="fas fa-bars"></i>
               </button>
               <div class="collapse navbar-collapse" id="navbarNav">
                 <ul style='font-size:19px;' class="navbar-nav">
