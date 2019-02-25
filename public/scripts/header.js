d7b6a263589fcbe38a34e44d2a8fa321380e9cf9
Asim <salimli.asim@gmail.com>
2019-02-25 03:46:52 +0000

    icon set replaced

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
   headerPartial.innerHTML = `
   <div id="header_menu_blue">
       <div class="float-right right-margin">
-       <a href='https://twitter.com/ANDOP12'> <img width="22px" src="https://s3.amazonaws.com/andop.org/social/twiter.png"> </a>
-          <a href='https://www.facebook.com/andib.aze/'> <img width="22px" src="https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/facebook-512.png"> </a>
-          <a href='https://www.youtube.com/channel/UC40q4m7-uv9pxDbP4qMpDsg?view_as=subscriber'> <img width="22px" src="https://s3.amazonaws.com/andop.org/social/youtube-logo.png"> </a>
-          <a href='https://www.linkedin.com/company/andop/'> <img width="22px" src="https://s3.amazonaws.com/andop.org/social/ln-logo.png"> </a>
-          <a href='https://www.instagram.com/sstm_baku/'> <img width="22px" src="https://instagram-brand.com/wp-content/uploads/2016/11/app-icon2.png"> </a>
+          <a href='https://twitter.com/ANDOP12'> <img width="22px" src="https://s3.amazonaws.com/andop.org/icons/tviter.png"> </a>
+          <a href='https://www.facebook.com/andib.aze/'> <img width="22px" src="https://s3.amazonaws.com/andop.org/icons/facebook.png"> </a>
+          <a href='https://www.youtube.com/channel/UC40q4m7-uv9pxDbP4qMpDsg?view_as=subscriber'> <img width="22px" src="https://s3.amazonaws.com/andop.org/icons/youyube.png"> </a>
+          <a href='https://www.linkedin.com/company/andop/'> <img width="22px" src="https://s3.amazonaws.com/andop.org/icons/linki.png"> </a>
+          <a href='https://www.instagram.com/sstm_baku/'> <img width="22px" src="https://s3.amazonaws.com/andop.org/icons/infta.png"> </a>
           <p style="display:inline; color: white!important;"><a style='color: white!important;' href='/contact.html'><span id="contactUs"></span></a></p>
           <select id="languageSelect" style="color:white; margin-left:10px; background-color:rgb(73,86,120); border:none; width:50px; height:100%;">
           <option id="ru">RU</option> <option id="az">AZ</option> <option id="eng">ENG</option>
