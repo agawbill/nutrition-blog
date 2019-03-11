d74243a1776096fa7ce124d91dab8cd69eef244b
Asim <salimli.asim@gmail.com>
2019-03-11 09:39:08 +0000

    icons restyling

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -217,19 +217,19 @@ document.addEventListener("DOMContentLoaded", () => {
               </ul>
               <ul style='font-size:19px;' class="navbar-nav ml-auto">
                 <li class="nav-item">
-                  <a href='https://www.facebook.com/andib.aze/'> <img width='22px' src='https://s3.amazonaws.com/andop.org/icons/facebook.png'> </a>
+                  <a href='https://www.facebook.com/andib.aze/'> <img style="width:15px;height:22px;margin-right:7px;"  src='https://s3.amazonaws.com/andop.org/icons/facebook.png'> </a>
                 </li>
                 <li class="nav-item active">
-                    <a href='https://twitter.com/ANDOP12'> <img width='22px' src='https://s3.amazonaws.com/andop.org/icons/tviter.png'> </a>
+                    <a href='https://twitter.com/ANDOP12'> <img style="width:22px;height:22px; margin-right:7px;"   src='https://s3.amazonaws.com/andop.org/icons/tviter.png'> </a>
                 </li>
                 <li class="nav-item">
-                  <a href='https://www.youtube.com/channel/UC40q4m7-uv9pxDbP4qMpDsg?view_as=subscriber'> <img width='22px' src='https://s3.amazonaws.com/andop.org/icons/youyube.png'> </a>
+                  <a href='https://www.youtube.com/channel/UC40q4m7-uv9pxDbP4qMpDsg?view_as=subscriber'> <img style="width:28px;height:22px; margin-right:7px;"   src='https://s3.amazonaws.com/andop.org/icons/youyube.png'> </a>
                 </li>
                 <li class="nav-item">
-                  <a href='https://www.linkedin.com/company/andop/'> <img width='22px' src='https://s3.amazonaws.com/andop.org/icons/linki.png'> </a>
+                  <a href='https://www.linkedin.com/company/andop/'> <img style="width:22px;height:22px; margin-right:7px;"  src='https://s3.amazonaws.com/andop.org/icons/linki.png'> </a>
                 </li>
                 <li class="nav-item">
-                  <a href='https://www.instagram.com/sstm_baku/'> <img width='22px' src='https://s3.amazonaws.com/andop.org/icons/infta.png'> </a>
+                  <a href='https://www.instagram.com/sstm_baku/'> <img style="width:22px;height:22px; margin-right:6px;" src='https://s3.amazonaws.com/andop.org/icons/infta.png'> </a>
                 </li>
               </ul>
             </div>
