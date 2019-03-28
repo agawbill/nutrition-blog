a27835085059d2400d88fe06df0dec96fff8b700
Asim <salimli.asim@gmail.com>
2019-03-28 09:11:43 +0000

    share buttons

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -31,12 +31,18 @@ const postLogic = item => {
   }
 };
 
-const postCycle = () => {
+const postCycle = async () => {
   item = post[0];
   postLogic(item);
-  var fbookButton = ``;
+  var fbookButton = await `<div class="share-btn" data-url="/post.html?id=${item._id}" data-title="..." data-desc="...">
+                					    <a class="btn-vk" data-id="vk"><i class="fab fa-vk"></i> VK</a>
+                					    <a class="btn-facebook" data-id="fb"><i class="fab fa-facebook-square"></i> Facebook</a>
+                					    <a class="btn-twitter" data-id="tw"><i class="fab fa-twitter"></i> Twitter</a>
+                					    <a class="btn-telegram" data-id="tg"><i class="fab fa-telegram"></i> Telegram</a>
+                					    <a class="btn-mail" data-id="mail"><i class="fas fa-at"></i> EMail</a>
+				                	</div>`;
 
-  const node = `
+  const node = await`
   <div class="col-lg-12 col-md-12 mb-12">
     <h4 class="card-title">
       <a href="/post.html?id=${item._id}">${title}</a>
@@ -47,19 +53,12 @@ const postCycle = () => {
     <p>
       <img src="${item.cover.url}" class="rounded" width="100%"> 
     </p>
-   
-    <div class="share-btn" data-url="/post.html?id=${item._id}" data-title="..." data-desc="...">
-                					    <a class="btn-vk" data-id="vk"><i class="fab fa-vk"></i> VK</a>
-                					    <a class="btn-facebook" data-id="fb"><i class="fab fa-facebook-square"></i> Facebook</a>
-                					    <a class="btn-twitter" data-id="tw"><i class="fab fa-twitter"></i> Twitter</a>
-                					    <a class="btn-telegram" data-id="tg"><i class="fab fa-telegram"></i> Telegram</a>
-                					    <a class="btn-mail" data-id="mail"><i class="fas fa-at"></i> EMail</a>
-		 </div>
-  
     <p>
     <h5>${converter.makeHtml(content)} </h5>
     </p>
-
+     <p>
+    ${fbookButton}
+    </p>
   </div>
     `;
   $("#postContainer").append(node + fbookButton);
