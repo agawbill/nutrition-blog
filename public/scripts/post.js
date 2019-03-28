5bbe7bf0e393a8cc1dfb7bb7335ec084f1e58ff3
Asim <salimli.asim@gmail.com>
2019-03-28 09:03:26 +0000

    share buttons

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -47,15 +47,15 @@ const postCycle = () => {
     <p>
       <img src="${item.cover.url}" class="rounded" width="100%"> 
     </p>
-    <p>
+   
     <div class="share-btn" data-url="/post.html?id=${item._id}" data-title="..." data-desc="...">
                 					    <a class="btn-vk" data-id="vk"><i class="fab fa-vk"></i> VK</a>
                 					    <a class="btn-facebook" data-id="fb"><i class="fab fa-facebook-square"></i> Facebook</a>
                 					    <a class="btn-twitter" data-id="tw"><i class="fab fa-twitter"></i> Twitter</a>
                 					    <a class="btn-telegram" data-id="tg"><i class="fab fa-telegram"></i> Telegram</a>
                 					    <a class="btn-mail" data-id="mail"><i class="fas fa-at"></i> EMail</a>
-				                	</div>
-    </p>
+		 </div>
+  
     <p>
     <h5>${converter.makeHtml(content)} </h5>
     </p>
