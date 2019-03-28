a4fd3a349125b69325e9356c5ee669c625cd28ed
Asim <salimli.asim@gmail.com>
2019-03-28 08:55:04 +0000

    share buttons

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -34,14 +34,13 @@ const postLogic = item => {
 const postCycle = () => {
   item = post[0];
   postLogic(item);
-  const fbookButton = `<div class="fb-share-button" data-href="https://www.facebook.com/sharer.php?u=http://andop.az/post.html?id=${
-    item._id
-  }" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a href="https://www.facebook.com/sharer.php?u=http://andop.az/post.html?id=${
-    item._id
-  }&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
-  </div>
-
-  `;
+  const fbookButton = `<div class="share-btn" data-url="/post.html?id=${item._id}" data-title="..." data-desc="...">
+                					    <a class="btn-vk" data-id="vk"><i class="fab fa-vk"></i> VK</a>
+                					    <a class="btn-facebook" data-id="fb"><i class="fab fa-facebook-square"></i> Facebook</a>
+                					    <a class="btn-twitter" data-id="tw"><i class="fab fa-twitter"></i> Twitter</a>
+                					    <a class="btn-telegram" data-id="tg"><i class="fab fa-telegram"></i> Telegram</a>
+                					    <a class="btn-mail" data-id="mail"><i class="fas fa-at"></i> EMail</a>
+				                	</div>`;
 
   const node = `
   <div class="col-lg-12 col-md-12 mb-12">
