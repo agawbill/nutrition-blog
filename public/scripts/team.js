5888b129db2cc7970337fcea1a7282982e71d072
Asim <salimli.asim@gmail.com>
2019-05-08 15:15:47 +0000

    right-left borders

--- public/scripts/team.js
+++ public/scripts/team.js
@@ -18,11 +18,11 @@ const allPostsRus =[];
                         </button>
 			        </div>
 			    </div>
-			    <div class="modal fade" id=`+`#`+modal+` tabindex="-1" role="dialog" aria-labelledby=`+ modal + `label` + ` aria-hidden="true">
+			    <div class="modal fade" id=`+`#`+modal+`tabindex="-1" role="dialog" aria-labelledby=`+ modal + `label` + `aria-hidden="true">
                   <div class="modal-dialog" role="document">
                     <div class="modal-content">
                       <div class="modal-header">
-                        <h5 class="modal-title" id=`+`#`+modal+`label`+`>Modal title</h5>
+                        <h5 class="modal-title" id=` + `#` + modal + `label` + `> Modal title </h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
                         </button>
@@ -36,8 +36,7 @@ const allPostsRus =[];
                       </div>
                     </div>
                   </div>
-                </div>
-			  `  
+                </div>`  
 		}
 		
 	for(let i=0;i<7;i++){
